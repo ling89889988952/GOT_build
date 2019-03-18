650338281fde5fbd9b71d3c5f507b08302642be6
ling89889988952 <zouling707@gmail.com>
2019-03-18 12:29:46 -0400

    add video function

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -5,23 +5,36 @@
 	// grab the shields at the bottom of the page
 	const    sigils      = document.querySelectorAll('.sigil-container'),
 		     lightbox    = document.querySelector('.lightbox'),
-		     video       = document.querySelector('video');
+			 video       = document.querySelector('video'),
+			 lbClose     = document.querySelector('.lightbox-close');
 
-	function showLightbox() {
-		lightbox.classList.add('show-lightbox');
+	function openLightbox() {
+		// debugger;
+		let targetHouse = this.className.split(" ")[1];
+
+		// this gives us a lowercase house name -> the second class on all of the shields ie stark, baratheon
+		// flip this to uppercase
+
+		let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetHouse}.mp4`;
+		lightbox.classList.add('lightbox-on');
+
+		video.load();
 		video.play();
 	}
 
-	function hideLightbox(){
-		lightbox.classList.remove('show-lightbox');
+	function closeLightbox(){
+		lightbox.classList.remove('lightbox-on');
 
 		// rewind the current video and pause it
 		video.currentTime = 0;
 		video.pause();
 	}
 
-	// sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
 
-	// video.addEventListener('ended', closeLightbox);
+	video.addEventListener('ended', closeLightbox);
+	lbClose.addEventListener('click', closeLightbox);
 
 })();
