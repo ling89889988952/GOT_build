4db482d626c5c8eeb7aa0d3fdd05dff05cfa871b
ling89889988952 <zouling707@gmail.com>
2019-04-12 00:11:48 -0400

    added function

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -9,10 +9,20 @@
 			 lbClose     = document.querySelector('.lightbox-close'),
 			 topBanners  = document.querySelector('#houseImages'),
 			 tagline     = document.querySelector('.house-name'),
-			 houseInfo     = document.querySelector('.house-info');
+			 houseInfo   = document.querySelector('.house-info');
+			 lbPlay      = document.querySelector('.lightbox-play');
+			 lbPause     = document.querySelector('.lightbox-pause');
+			 mute      = document.querySelector('.mute');
+			 volume      = document.querySelector('.volume');
+
 
 	//  this is a container for some dynamic data
 	const houseData = [ 
+
+	[`BARATHEON`, `House Baratheon of Storm's End is a legally extinct Great House of Westeros. A cadet 
+	branch was formerly the royal house, but House Lannister now controls the throne. House Baratheon 
+	traditionally ruled the Stormlands on the eastern coast of Westeros, aptly named for its frequent 
+	storms, from their seat of Storm's End.`],
 
 	[`STARK` , `House Stark of Winterfell is a Great House of Westeros,ruling over the vast region known as 
 		the North from their seat in Winterfell.It is one of the oldest lines of Westerosi nobility by 
@@ -20,22 +30,10 @@
 		conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros,
 		the leaders of House Stark ruled over the region as the Kings in the North.`],
 
-	[`BARATHEON`, `House Baratheon of Storm's End is a legally extinct Great House of Westeros. A cadet 
-	branch was formerly the royal house, but House Lannister now controls the throne. House Baratheon 
-	traditionally ruled the Stormlands on the eastern coast of Westeros, aptly named for its frequent 
-	storms, from their seat of Storm's End.`],
-
 	[`LANNISTER`, `House Lannister of Casterly Rock is one of the Great Houses of Westeros, one of its 
 	richest and most powerful families and oldest dynasties. It is also the current royal house of the 
 	Seven Kingdoms following the extinction of House Baratheon of King's Landing, which had been their 
 	puppet house anyway.`],
-
-	[`GREYJOY`,`House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands,
-	 a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. 
-	 The head of the house is the Lord Reaper of Pyke.
-	House Greyjoy's sigil is traditionally a golden kraken on a black field. Their house words are "We Do Not 
-	Sow," although the phrase "What Is Dead May Never Die" is also closely associated with House Greyjoy and 
-	their bannermen, as they are associated with the faith of the Drowned God. `],
 
 	[`TULLY`, `House Tully of Riverrun is an exiled Great House of Westeros. Its most senior member carried
 	 the title of Lord of Riverrun and Lord Paramount of the Trident, until the Red Wedding. The current head 
@@ -48,7 +46,7 @@
 	  ad of House Arryn is Robin Arryn, the Lord of the Eyrie, with his stepfather Petyr Baelish acting as Lord
 	   Protector until he reaches the age of majority. `],
 
-	[`TARGERYEN`, `House Targaryen of Dragonstone is a Great House of Westeros and was the ruling royal House 
+	[`TARGARYEN`, `House Targaryen of Dragonstone is a Great House of Westeros and was the ruling royal House 
 	of the Seven Kingdoms for three centuries since it conquered and unified the realm, before it was deposed
 	 during Robert's Rebellion and House Baratheon replaced it as the new royal House. The few surviving 
 	 Targaryens fled into exile to the Free Cities of Essos across the Narrow Sea. Currently based on 
@@ -62,6 +60,20 @@
 	 for the Kingdom of the North. The current head of the house is unknown following the assassinations 
 	 	of Lord Walder Frey and two of his sons, Lothar Frey and Walder Rivers, by the vengeful Arya Stark. 
 	 This is made more complex by the subsequent assassination of all the male Freys soon after.`],
+
+
+	[`TYREEL`,`House Tyrell of Highgarden is an extinct Great House of Westeros. It ruled over the Reach, 
+	a vast, fertile, and heavily-populated region of southwestern Westeros, from their castle-seat of Highgarden
+	 as Lords Paramount of the Reach and Wardens of the South after taking control of the region from House Gardener 
+	 during the Targaryen conquest. `],
+
+	[`GREYJOY`,`House Greyjoy of Pyke is one of the Great Houses of Westeros. It rules over the Iron Islands,
+	 a harsh and bleak collection of islands off the west coast of Westeros, from the castle at Pyke. 
+	 The head of the house is the Lord Reaper of Pyke.
+	House Greyjoy's sigil is traditionally a golden kraken on a black field. Their house words are "We Do Not 
+	Sow," although the phrase "What Is Dead May Never Die" is also closely associated with House Greyjoy and 
+	their bannermen, as they are associated with the faith of the Drowned God. `],
+
 
 
 	];
@@ -80,7 +92,7 @@
 		tagline.textContent = `House ${targetVid}`; 
 
 		// populate the house content
-		houseInfo.textContent = houseData[0];
+		houseInfo.textContent = houseData[0][1];
 
 		video.src = `video/house-${targetHouse}.mp4`;
 		lightbox.classList.add('lightbox-on');
@@ -97,7 +109,43 @@
 		video.pause();
 	}
 
+	function pauseVideo() {
+		if (video.play){
+			video.pause();
+
+			console.log("pause");
+		
+		}
+	}
+
+
+	function playVideo() {
+		if (video.pause){
+			video.play();
+
+			console.log("play");
+		}
+	}
+  	
+
+  	function videoMute(){
+  		if (video.muted) {
+  			video.muted = false;
+  			mute.innterHTML = "Mute";
+  		}else{
+  			video.muted = ture;
+  			mute.innterHTML = "Unmute";
+  		}
+  	}
+
+  	
+
+  	function setVolume(){
+  		video.volume = volume.value / 100;
+  	}
+
 	function animationBanners(){
+
 		// move the banners to the left so that the current house banner is move
 		const offSet = 600;
 
@@ -111,17 +159,24 @@
 
 		// change the text content on the page per house
 		tagline.textContent = `House ${houseData[0][0]}`;
-		houseInfo.textContent = houseData[0][1];
+		houseInfo.textContent = `House ${houseData[0][1]}`;
 
 
 	}
 
 	// function {
-		sigils .forEach(sigil => sigil.addEventListener('click', animationBanners));
+	sigils .forEach(sigil => sigil.addEventListener('click', animationBanners));
 
 	// sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
 	sigils .forEach(sigil => sigil.addEventListener('click', openLightbox));
 	video.addEventListener('ended', closeLightbox);
 	lbClose.addEventListener('click', closeLightbox);
+	lbPlay.addEventListener('click',playVideo);
+	lbPause.addEventListener('click',pauseVideo);
+	mute.addEventListener('click',videoMute);
+	volume.addEventListener('change',setVolume);
+
+
+
 
 })();
